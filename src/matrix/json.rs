37f7779941f7c422972df675710675580e7488c6
Torrie Fischer <tdfischer@hackerbots.net>
2016-01-05 01:34:36 -0800

    matrix: json: provide more useful errors with bad paths

--- src/matrix/json.rs
+++ src/matrix/json.rs
@@ -4,15 +4,24 @@ pub fn path<'a>(json: &'a Json, path: &str) -> &'a Json {
     let parts = path.split(".");
     let mut cur = json;
     for p in parts {
-        cur = cur.as_object().unwrap().get(p).unwrap();
+        cur = match cur.as_object().unwrap().get(p) {
+            Some(c) => c,
+            None => panic!("Could not find {} in {:?} (lost at {})", path, json, p)
+        }
     }
     cur
 }
 
 pub fn array<'a>(json: &'a Json, path: &str) -> &'a Array {
-    self::path(json, path).as_array().unwrap()
+    match self::path(json, path).as_array() {
+        Some(p) => p,
+        None => panic!("{} in {:?} is not an array", path, json)
+    }
 }
 
 pub fn string<'a>(json: &'a Json, path: &str) -> &'a str{
-    self::path(json, path).as_string().unwrap()
+    match self::path(json, path).as_string() {
+        Some(p) => p,
+        None => panic!("{} in {:?} is not an array", path, json)
+    }
 }
