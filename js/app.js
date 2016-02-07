560ce8c82836a960c96f0252cc374c1977796ab0
Torrie Fischer <tdfischer@hackerbots.net>
2016-02-07 15:27:36 -0800

    update Get It Now docs and show latest version on site

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+
+$(document).ready(function() {
+  $.ajax({
+    url: "https://api.github.com/repos/tdfischer/pto/releases/latest",
+    success: function(data) {
+      console.log(data);
+      $('#version').html(data.name);
+    }
+  });
+});
